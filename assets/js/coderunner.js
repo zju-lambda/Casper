38f7a036e7d765b9bf9e1f5d15806ac51329149b
ljw <nicekingwei@sina.com>
2018-03-20 15:43:23 +0800

    math

--- assets/js/coderunner.js
+++ assets/js/coderunner.js
@@ -1,10 +1,9 @@
-$("pre").find("code").parent().after('<p class="run" style="text-align:right"><a href="#" class="run">Run</a></p><blockquote class="run" style="display:none"></blockquote>');
+$("pre").find("code").parent().after('<p class="run" style="display:none"><a href="javascript:void(0)" class="run">Run</a></p><blockquote class="run" style="display:none"></blockquote>');
 var as=$("a.run"),bs=$("blockquote.run"),cs=$("pre code"),ds=cs.parent();
 var langs4e = [],langs4s = [],editors = [];
-ds.addClass("editor");  
+  
 for(i=0;i<as.length;i++){
     as[i].setAttribute("onclick","sendCode("+i+")");
-    as[i].setAttribute("href","#code"+i+"");
     var ss = cs[i].classList;
     var flag = true;
     for(j=0;j<ss.length;j++) if(flag){
@@ -12,7 +11,7 @@ for(i=0;i<as.length;i++){
         var m = s.match("language-(.+)");
         if(m){
             flag = false;
-            switch(m[1]){
+            switch(m[1].toLowerCase()){
             case "c":
                 langs4s[i] = "c";
                 langs4e[i] = "c_cpp";
@@ -24,7 +23,7 @@ for(i=0;i<as.length;i++){
                 langs4s[i] = "hs";
                 langs4e[i] = "haskell";
                 break;
-            case "javascript":
+            case "javascript":case "js":
                 langs4s[i] = "js";
                 langs4e[i] = "javascript";
                 break;
@@ -47,21 +46,22 @@ for(i=0;i<as.length;i++){
             case "lua":
                 langs4e[i]=langs4s[i]="lua";
                 break;
-            default:
-                as[i].setAttribute("style","display:none");
-                break;
             }
+            if(m[1]!=m[1].toLowerCase()) langs4s[i]="";
         }
     }
-    editors[i] = ace.edit(ds[i]);
-    ds[i].setAttribute("id","code"+i);
-    editors[i].setAutoScrollEditorIntoView(true);
-    editors[i].setOption("maxLines", 30);   
-    editors[i].setValue("\n"+editors[i].getValue());
-    editors[i].setTheme("ace/theme/ambiance");
-    editors[i].session.setMode("ace/mode/"+langs4e[i]);
-    editors[i].renderer.setShowGutter(false);
-    editors[i].selection.moveCursorFileStart();
+    if(langs4e[i]){
+        if(langs4s[i]) as[i].setAttribute("style","text-align:right");
+        editors[i] = ace.edit(ds[i]);
+        ds[i].setAttribute("id","code"+i);
+        editors[i].setAutoScrollEditorIntoView(true);
+        editors[i].setOption("maxLines", 1000);   
+        editors[i].setValue("\n"+editors[i].getValue());
+        editors[i].setTheme("ace/theme/ambiance");
+        editors[i].session.setMode("ace/mode/"+langs4e[i]);
+        editors[i].renderer.setShowGutter(false);
+        editors[i].selection.moveCursorFileStart();
+    }
 }
 
 function sendCode(i){
@@ -69,7 +69,7 @@ function sendCode(i){
     var lang = langs4s[i];
 
     bs[i].setAttribute("style","display:block");
-    bs[i].textContent = "running...";
+    bs[i].textContent = "Running...";
 
     $.ajax({
         'url':"http://123.206.103.187:2468/",
@@ -77,10 +77,10 @@ function sendCode(i){
         'contentType':'application/json',
         'data':JSON.stringify({"lang":lang,"code":code}),
         'error':function(xhr){
-            bs[i].textContent = "error";
+            bs[i].textContent = "Error";
         },
         'success':function(response){
-            bs[i].textContent = "result:\n" + response;
+            bs[i].textContent = "Result:\n" + response;
         }
     });
 }
